a708c2af8ffde83ee8c7169d544f7a1ed2a388ca
Paweł <emotka90sima@gmail.com>
2023-10-23 16:15:03 +0200

    new views system and 404 error

--- api/app.js
+++ api/app.js
@@ -12,33 +12,32 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
-app.use(cors());
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-var sess = {
-  secret: 'sdflksdklfjlkjklip',
-  cookie: {}
-}
-
-app.set('trust proxy', 1) // trust first proxy
-sess.cookie.secure = true // serve secure cookies
-
-
-app.use(session(sess))
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false,
+  cookie: { maxAge: 60000, secure: false }
+}))
+app.use(cors());
 app.use("/api", APIRouter);
 
+
+
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.all("*", (req, res ) =>{
+    res.status(404).render("error") 
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
