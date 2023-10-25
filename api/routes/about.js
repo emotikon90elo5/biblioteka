2f6bb2437bf5ece48453f8d757b77b77e2590b10
Paweł <emotka90sima@gmail.com>
2023-10-25 21:57:40 +0200

    Change nav style

--- api/routes/about.js
+++
@@ -1,9 +0,0 @@
-var express = require("express");
-var router = express.Router();
-require('dotenv').config();
-
-router.get("/about", (req, res) => {
-  res.render("about")
-})
-
-module.exports = router;
--- api/routes/main.js
+++ api/routes/main.js
@@ -1,9 +1,21 @@
 var express = require("express");
 var router = express.Router();
 require('dotenv').config();
+var mysql = require("mysql")
 
-router.get("/", (req, res) => {
+var con = mysql.createConnection({
+  host: process.env.MYSQLhost,
+  user: process.env.MYSQLuser,
+  password: process.env.MYSQLpassword,
+  database: process.env.MYSQLdatabase
+});
+
+router.get("/",  (req, res) => {
   res.render("main")
 })
 
+router.get("/about", (req, res) => {
+  res.render("about")
+  
+})
 module.exports = router;
