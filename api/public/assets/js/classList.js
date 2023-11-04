ec3ab8e53d45034b8661f377ead556f81f79fb45
kubi2392 <121381391+kubi2392@users.noreply.github.com>
2023-11-04 18:39:39 +0100

    classlist.ejs/js

---
+++ api/public/assets/js/classList.js
@@ -0,0 +1,69 @@
+let closePopup = document.getElementById("closePopup");
+let PopupCancel = document.getElementById("Cancel");
+let PopupCommit = document.getElementById("Commit");
+async function shelf() {
+  let table = document.getElementById("table");
+  let popupText = document.getElementById("exampleModalLongTitleContent");
+  const response = await fetch(`http://localhost:4000/api/config/class`, {
+    credentials: "include",
+  });
+  let jsona = await response.json();
+  
+  await jsona.data.forEach((e) => {
+    let tr = document.createElement("tr");
+
+    let td1 = document.createElement("td");
+    td1.innerText = e.name;
+    tr.append(td1);
+
+    let td2 = document.createElement("td");
+    td2.innerText = e._count.pupils;
+    tr.append(td2);
+
+    let td4 = document.createElement("td");
+
+    let button = document.createElement("a");
+
+    button.onclick = () => {
+      popupRemove();
+    };
+    function popupRemove() {
+      popupText.innerHTML = `<b>${e.title}</b>`;
+
+      $("#exampleModalCenter").modal("show");
+    }
+    let remove = document.createElement("i");
+    remove.classList.add("ti");
+    remove.classList.add("ti-trash");
+    button.append(remove);
+    td4.append(button);
+
+    let updateHref = document.createElement("a");
+    updateHref.href = `/admin/pupilList/${e.id}`;
+    let update = document.createElement("i");
+    update.classList.add("ti");
+    update.classList.add("ti-pencil");
+    updateHref.append(update);
+    td4.append(updateHref);
+
+    tr.append(td4);
+
+    table.append(tr);
+  });
+}
+shelf();
+
+closePopup.addEventListener("click", () => {
+  $("#exampleModalCenter").modal("hide");
+});
+PopupCancel.addEventListener("click", () => {
+  $("#exampleModalCenter").modal("hide");
+});
+ PopupCommit.addEventListener("click", async() => {
+  $("#exampleModalCenter").modal("hide");
+  const response = await fetch("http://localhost:4000/api/menage/delate/book", {
+      credentials: "include",
+      method:"delete"
+    });
+    
+});
--- api/routes/apis/config.js
+++ api/routes/apis/config.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { PrismaClient, Prisma } = require('@prisma/client');
-const errHanler = require("../errorHandler")
-const prisma = new PrismaClient()
-const notNumbers = /\D/g
+const { PrismaClient, Prisma } = require("@prisma/client");
+const errHanler = require("../errorHandler");
+const prisma = new PrismaClient();
+const notNumbers = /\D/g;
 
 router.use(express.json());
 
@@ -12,156 +12,158 @@ router.get("/class", async ({ session: { SchoolID } }, res) => {
   try {
     classList = await prisma.class.findMany({
       where: {
-        schoolsId: SchoolID
-      }
-    })
+        schoolsId: SchoolID,
+      },
+      select: {
+        id:true,
+        name:true,
+        _count: {
+          select: {
+            pupils: true,
+          },
+        },
+      },
+    });
   } catch (err) {
-    return res.json({ succes: false })
+    return res.json({ succes: false });
   }
-  if (classList == null) return res.json({ succes: false })
-  return res.json({ succes: true, data: classList })
-})
+  if (classList == null) return res.json({ succes: false });
+  return res.json({ succes: true, data: classList });
+});
 
-router.get("/types", async ({ }, res) => {
+router.get("/types", async ({}, res) => {
   let types;
   try {
-    types = await prisma.type.findMany({})
+    types = await prisma.type.findMany({});
   } catch (err) {
-    return res.json({ succes: false })
+    return res.json({ succes: false });
   }
-  if (types == null) return res.json({ succes: false })
-  return res.json({ succes: true, data: types })
-})
+  if (types == null) return res.json({ succes: false });
+  return res.json({ succes: true, data: types });
+});
 
 router.get("/shelf", async ({ session: { SchoolID } }, res) => {
   let shelf;
   try {
     shelf = await prisma.bookcases.findMany({
       where: {
-        schoolsId: SchoolID
+        schoolsId: SchoolID,
       },
       include: {
         shelves: {
           select: {
             id: true,
-            name: true
-          }
-        }
-      }
-    })
+            name: true,
+          },
+        },
+      },
+    });
   } catch (err) {
-    return res.json({ succes: false })
+    return res.json({ succes: false });
   }
-  if (shelf == null) return res.json({ succes: false })
-  return res.json({ succes: true, data: shelf })
-})
+  if (shelf == null) return res.json({ succes: false });
+  return res.json({ succes: true, data: shelf });
+});
 router.get("/bookcase", async ({ session: { SchoolID } }, res) => {
-
   let bookcases;
   try {
     bookcases = await prisma.bookcases.findMany({
       where: {
-        schoolsId: SchoolID
-      }
-    })
+        schoolsId: SchoolID,
+      },
+    });
   } catch (err) {
-    return res.json({ succes: false })
+    return res.json({ succes: false });
   }
-  if (bookcases == null) return res.json({ succes: false })
-  return res.json({ succes: true, data: bookcases })
-
-})
+  if (bookcases == null) return res.json({ succes: false });
+  return res.json({ succes: true, data: bookcases });
+});
 router.get("/books", async ({ session: { SchoolID } }, res) => {
-  let books
+  let books;
   try {
     books = await prisma.books.findMany({
       where: {
         shelf: {
           bookcase: {
-            schoolsId: SchoolID
-          }
-        }
-      }
-    })
+            schoolsId: SchoolID,
+          },
+        },
+      },
+    });
+  } catch (err) {
+    return res.json({ succes: false });
   }
-  catch (err) {
-    return res.json({ succes: false })
-  }
-  if (books == null) return res.json({ succes: false })
+  if (books == null) return res.json({ succes: false });
   res.json({
-    succes: true, data: books
-  })
-})
+    succes: true,
+    data: books,
+  });
+});
 
 router.get("/pupils", async ({ session: { SchoolID } }, res) => {
-
   let pupils;
   try {
     pupils = await prisma.pupils.findMany({
       where: {
         class: {
-          schoolsId:SchoolID
-        }
-        
-      }
-      ,include:{
-        class:true
-      }
-    })
+          schoolsId: SchoolID,
+        },
+      },
+      include: {
+        class: true,
+      },
+    });
   } catch (err) {
-    console.log(err)
-    return res.json({ succes: false })
-    
+    console.log(err);
+    return res.json({ succes: false });
   }
-  if (pupils == null) return res.json({ succes: false })
-  return res.json({ succes: true, data: pupils })
-
-})
-router.get("/pupils/:id", async ({ session: { SchoolID },params:{id} }, res) => {
-
-  let pupils;
-  try {
-    pupils = await prisma.pupils.findFirst({
-      where: {
-        id:Number(id.replace(notNumbers ,"")),
-        class: {
-          schoolsId:SchoolID
-        }
-        
-      }
-      ,include:{
-        class:true
-      }
-    })
-  } catch (err) {
-    console.log(err)
-    return res.json({ succes: false })
-    
+  if (pupils == null) return res.json({ succes: false });
+  return res.json({ succes: true, data: pupils });
+});
+router.get(
+  "/pupils/:id",
+  async ({ session: { SchoolID }, params: { id } }, res) => {
+    let pupils;
+    try {
+      pupils = await prisma.pupils.findFirst({
+        where: {
+          id: Number(id.replace(notNumbers, "")),
+          class: {
+            schoolsId: SchoolID,
+          },
+        },
+        include: {
+          class: true,
+        },
+      });
+    } catch (err) {
+      console.log(err);
+      return res.json({ succes: false });
+    }
+    if (pupils == null) return res.json({ succes: false });
+    return res.json({ succes: true, data: pupils });
   }
-  if (pupils == null) return res.json({ succes: false })
-  return res.json({ succes: true, data: pupils })
-
-})
-router.get("/classpupils/:id", async ({ session: { SchoolID },params:{id} }, res) => {
-
-  let pupils;
-  try {
-    pupils = await prisma.class.findFirst({
-      where: {
-        id:Number(id.replace(notNumbers ,"")),
-          schoolsId:SchoolID
-      }
-      ,include:{
-        pupils:true
-      }
-    })
-  } catch (err) {
-    console.log(err)
-    return res.json({ succes: false })
-    
+);
+router.get(
+  "/classpupils/:id",
+  async ({ session: { SchoolID }, params: { id } }, res) => {
+    let pupils;
+    try {
+      pupils = await prisma.class.findFirst({
+        where: {
+          id: Number(id.replace(notNumbers, "")),
+          schoolsId: SchoolID,
+        },
+        include: {
+          pupils: true,
+        },
+      });
+    } catch (err) {
+      console.log(err);
+      return res.json({ succes: false });
+    }
+    if (pupils == null) return res.json({ succes: false });
+    return res.json({ succes: true, data: pupils });
   }
-  if (pupils == null) return res.json({ succes: false })
-  return res.json({ succes: true, data: pupils })
-
-})
+);
 module.exports = router;
