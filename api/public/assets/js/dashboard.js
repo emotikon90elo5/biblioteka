d4f92cdb99b5aff9dd1112b3847f7e8c91db3566
Paweł <emotka90sima@gmail.com>
2023-11-06 18:45:29 +0100

    Remove unnecessary js from dashboard.js

--- api/public/assets/js/dashboard.js
+++ api/public/assets/js/dashboard.js
@@ -129,115 +129,115 @@ const renderChart = async () => {
   chart.render();
 }
 
-$(function () {
-  // =====================================
-  // Profit
-  // =====================================
+// $(function () {
+//   // =====================================
+//   // Profit
+//   // =====================================
 
 
 
-  // =====================================
-  // Breakup
-  // =====================================
-  var breakup = {
-    color: "#adb5bd",
-    series: [10000],
-    labels: ["2023"],
-    chart: {
-      width: 180,
-      type: "donut",
-      fontFamily: "Plus Jakarta Sans', sans-serif",
-      foreColor: "#adb0bb",
-    },
-    plotOptions: {
-      pie: {
-        startAngle: 0,
-        endAngle: 360,
-        donut: {
-          size: '75%',
-        },
-      },
-    },
-    stroke: {
-      show: false,
-    },
+//   // =====================================
+//   // Breakup
+//   // =====================================
+//   var breakup = {
+//     color: "#adb5bd",
+//     series: [10000],
+//     labels: ["2023"],
+//     chart: {
+//       width: 180,
+//       type: "donut",
+//       fontFamily: "Plus Jakarta Sans', sans-serif",
+//       foreColor: "#adb0bb",
+//     },
+//     plotOptions: {
+//       pie: {
+//         startAngle: 0,
+//         endAngle: 360,
+//         donut: {
+//           size: '75%',
+//         },
+//       },
+//     },
+//     stroke: {
+//       show: false,
+//     },
 
-    dataLabels: {
-      enabled: false,
-    },
+//     dataLabels: {
+//       enabled: false,
+//     },
 
-    legend: {
-      show: false,
-    },
-    colors: ["#5D87FF", "#ecf2ff", "#F9F9FD"],
+//     legend: {
+//       show: false,
+//     },
+//     colors: ["#5D87FF", "#ecf2ff", "#F9F9FD"],
 
-    responsive: [
-      {
-        breakpoint: 991,
-        options: {
-          chart: {
-            width: 150,
-          },
-        },
-      },
-    ],
-    tooltip: {
-      theme: "dark",
-      fillSeriesColor: false,
-    },
-  };
+//     responsive: [
+//       {
+//         breakpoint: 991,
+//         options: {
+//           chart: {
+//             width: 150,
+//           },
+//         },
+//       },
+//     ],
+//     tooltip: {
+//       theme: "dark",
+//       fillSeriesColor: false,
+//     },
+//   };
 
-  var chart = new ApexCharts(document.querySelector("#breakup"), breakup);
-  chart.render();
+//   var chart = new ApexCharts(document.querySelector("#breakup"), breakup);
+//   chart.render();
 
 
 
   // =====================================
   // Earning
   // =====================================
-  var earning = {
-    chart: {
-      id: "sparkline3",
-      type: "area",
-      height: 60,
-      sparkline: {
-        enabled: true,
-      },
-      group: "sparklines",
-      fontFamily: "Plus Jakarta Sans', sans-serif",
-      foreColor: "#adb0bb",
-    },
-    series: [
-      {
-        name: "Earnings",
-        color: "#49BEFF",
-        data: [25, 66, 20, 40, 12, 58, 20],
-      },
-    ],
-    stroke: {
-      curve: "smooth",
-      width: 2,
-    },
-    fill: {
-      colors: ["#f3feff"],
-      type: "solid",
-      opacity: 0.05,
-    },
+  // var earning = {
+  //   chart: {
+  //     id: "sparkline3",
+  //     type: "area",
+  //     height: 60,
+  //     sparkline: {
+  //       enabled: true,
+  //     },
+  //     group: "sparklines",
+  //     fontFamily: "Plus Jakarta Sans', sans-serif",
+  //     foreColor: "#adb0bb",
+  //   },
+  //   series: [
+  //     {
+  //       name: "Earnings",
+  //       color: "#49BEFF",
+  //       data: [25, 66, 20, 40, 12, 58, 20],
+  //     },
+  //   ],
+  //   stroke: {
+  //     curve: "smooth",
+  //     width: 2,
+  //   },
+  //   fill: {
+  //     colors: ["#f3feff"],
+  //     type: "solid",
+  //     opacity: 0.05,
+  //   },
 
-    markers: {
-      size: 0,
-    },
-    tooltip: {
-      theme: "dark",
-      fixed: {
-        enabled: true,
-        position: "right",
-      },
-      x: {
-        show: false,
-      },
-    },
-  };
-  new ApexCharts(document.querySelector("#earning"), earning).render();
-})
+  //   markers: {
+  //     size: 0,
+  //   },
+  //   tooltip: {
+  //     theme: "dark",
+  //     fixed: {
+  //       enabled: true,
+  //       position: "right",
+  //     },
+  //     x: {
+  //       show: false,
+  //     },
+  //   },
+  // };
+  // new ApexCharts(document.querySelector("#earning"), earning).render();
+// })
 renderChart()
